984bd4ecc7a136c783757330aedc5fe3dccffa44
Rozhko Alexey <a.rozhko@itpdevelopment.ru>
2021-12-21 16:20:21 +0300

    stage

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace swTestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(30 >= 15 && 10 <= 15 || true /*&& 10 > 5*/);
+           
+        }
+    }
+}
